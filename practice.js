08e17cf658f867045220a346cd60946a8efa6910
Jarif Ahmed <your_github_email@gmail.com>
2026-02-07 11:15:29 +0600

    added all file updates

---
+++ practice.js
@@ -0,0 +1,64 @@
+// const numbers = [1, 2, 3, 4, 5];
+
+// const doubledNumbers = numbers.map(num => num * 2);
+// // console.log(doubledNumbers)
+
+// const names = ["jarif", "ahmed", "rahim"];
+// const upperCaseNames = names.map(name => name.toUpperCase());
+// // console.log(upperCaseNames)
+
+// const prices = [10, 25, 40, 100];
+
+// const updatedPrices = prices.map(price => price + 5);
+// // console.log(updatedPrices)
+
+
+// const ArrNumbers = [345, 456,33, 7, 88];
+
+// const newArr = ArrNumbers.filter(number => ArrNumbers > 10);
+// console.log(newArr);
+
+
+const number = [5, 12, 8, 20, 3, 15];
+const greaterThanTen = number.filter(num => num >10);
+// console.log(greaterThanTen);
+
+
+const ages = [12, 18, 25, 16, 30, 14];
+const adults = ages.filter(age => age >= 18);
+// console.log(adults);
+
+
+const words = ["apple", "cat", "banana", "dog", "grapes"];
+const longWords = words.filter(word => word.length > 4);
+// console.log(longWords);
+
+
+const fruits = ["apple", "banana", "mango", "orange"];
+// fruits.forEach(fruit => {
+//     console.log(`I like ${fruit}`);
+// });
+
+// const numbers = [10, 20, 30, 40];
+// let total = 0;
+
+// numbers.forEach(num => {
+//     total += num;
+// });
+
+// console.log(total);
+
+
+// const numbers = [3, 7, 10, 12, 15, 20];
+// const result = numbers.find(num => num % 5 === 0);
+// console.log(result);
+
+
+const people = [
+    { name: "Ali", age: 17 },
+    { name: "Sara", age: 22 },
+    { name: "Jhon", age: 19 }
+];
+
+const adultsPerson = people.find(person => person.age > 18);
+console.log(adultsPerson);
